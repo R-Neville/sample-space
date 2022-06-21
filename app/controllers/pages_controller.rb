0edc1d957914c39eff809e3da71c3a394c23aed2
Robert Neville <gcab012204@coderacademy.edu.au>
2022-06-21 14:40:08 +1000

    Create views for remaining pages (pages controller)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def browse
+  end
+
+  def creators
+  end
+
+  def about
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get '/browse', to: 'pages#browse'
+  get '/creators', to: 'pages#creators'
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
 end

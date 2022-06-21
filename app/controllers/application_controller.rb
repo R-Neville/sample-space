e31ffbb8fc364acd68e4968e8bcb1e06b7e27207
Robert Neville <gcab012204@coderacademy.edu.au>
2022-06-21 19:51:53 +1000

    Update app controller to accomodate custom user fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def initialize
     super
 
@@ -20,5 +22,11 @@ class ApplicationController < ActionController::Base
         url: Rails.application.routes.url_helpers.contact_path
       }
     ]
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
   end
 end

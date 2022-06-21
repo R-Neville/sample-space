7d74556338c900bd9a5da159a38f8881472b4239
Robert Neville <gcab012204@coderacademy.edu.au>
2022-06-21 15:30:38 +1000

    Update nav link rendering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  def initialize
+    super
+
+    @nav_links = [
+      {
+        text: 'browse',
+        url: Rails.application.routes.url_helpers.browse_path
+      },
+      {
+        text: 'creators',
+        url: Rails.application.routes.url_helpers.creators_path
+      },
+      {
+        text: 'about',
+        url: Rails.application.routes.url_helpers.about_path
+      },
+      {
+        text: 'contact',
+        url: Rails.application.routes.url_helpers.contact_path
+      }
+    ]
+  end
 end

c20535b9382c9f6033e98f5c2df39a79e3c92d4f
Robert Neville <gcab012204@coderacademy.edu.au>
2022-06-22 14:32:46 +1000

    Add creator checkbox to edit account page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,6 +30,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :username, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :username, :first_name, :last_name, :is_creator])
   end
 end

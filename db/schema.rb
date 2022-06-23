41613b1902acd71ff07f24011cc4fd00aa39cdf7
Robert Neville <gcab012204@coderacademy.edu.au>
2022-06-23 16:03:26 +1000

    Add public field to samples

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_23_043900) do
+ActiveRecord::Schema.define(version: 2022_06_23_055958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2022_06_23_043900) do
     t.float "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "public"
     t.index ["user_id"], name: "index_samples_on_user_id"
   end
 

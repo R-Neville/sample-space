6e50603f68ac75d8f24bbbf724e3ae81a03f1345
Robert Neville <gcab012204@coderacademy.edu.au>
2022-06-24 11:47:40 +1000

    Remove private samples from show tag page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,6 +5,7 @@ class TagsController < ApplicationController
   def show
     @samples = Sample.tagged_with(params[:tag])
     if @samples.length > 0
+      @samples = @samples.select { |sample| sample.is_public }
       @tag = params[:tag]
     end
   end

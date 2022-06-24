4ba02903d63a3051590ff6e1e8a67d3a12376979
Robert Neville <gcab012204@coderacademy.edu.au>
2022-06-24 12:33:52 +1000

    Generate download model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_23_095151) do
+ActiveRecord::Schema.define(version: 2022_06_24_023210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 2022_06_23_095151) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "downloads", force: :cascade do |t|
+    t.bigint "sample_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sample_id"], name: "index_downloads_on_sample_id"
+    t.index ["user_id"], name: "index_downloads_on_user_id"
   end
 
   create_table "samples", force: :cascade do |t|
@@ -108,6 +117,8 @@ ActiveRecord::Schema.define(version: 2022_06_23_095151) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "downloads", "samples"
+  add_foreign_key "downloads", "users"
   add_foreign_key "samples", "users"
   add_foreign_key "taggings", "tags"
 end

464dc14a9d5cde9d3b30384d492d1f04423a95f1
Robert Neville <gcab012204@coderacademy.edu.au>
2022-06-28 14:00:00 +1000

    Implement like notifications

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,6 +5,16 @@ class LikesController < ApplicationController
     like = Like.where(sample_id: params[:sample_id], user_id: current_user.id).first
     if !like
       Like.create(sample_id: params[:sample_id], user_id: current_user.id)
+      
+      sample = Sample.find(params[:sample_id])
+      for_user = User.find(sample.user_id)
+      
+      Notification.create(
+        for: for_user,
+        from: current_user,
+        sample_id: sample.id,
+        notification_type: 'like'
+      )
     end
     redirect_to show_sample_path(params[:sample_id])
   end

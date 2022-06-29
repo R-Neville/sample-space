ad352633ecf6e8e0495927d67f11fa5bc183f448
Robert Neville <gcab012204@coderacademy.edu.au>
2022-06-29 14:35:06 +1000

    Add recent uploads and popular tags to browse page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,20 @@ class PagesController < ApplicationController
   end
 
   def browse
-    
+    @popular_tags = ActsAsTaggableOn::Tag.most_used(20)
+    recent_uploads = Sample.where(is_public: true).order("created_at").last(10)
+    @recent_uploads_info = []
+    recent_uploads.each do |upload|
+      likes = upload.likes.all.count
+      creator = User.where(id: upload.user_id).first
+      download_count = upload.downloads.all.count
+      @recent_uploads_info.push({
+        sample: upload, 
+        creator: creator, 
+        likes: likes, 
+        downloads: download_count
+      })
+    end
   end
 
   def about
